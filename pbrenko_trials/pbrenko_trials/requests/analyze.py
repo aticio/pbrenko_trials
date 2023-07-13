c29f62df29bb15e755c0dabf5789ff349718e384
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2023-07-13 09:51:09 +0000

    request object for analyze created with proper tests

---
+++ pbrenko_trials/pbrenko_trials/requests/analyze.py
@@ -0,0 +1,45 @@
+class AnalyzeCreateInvalidRequest:
+    def __init__(self):
+        self.errors = []
+
+    def add_error(self, param, message):
+        self.errors.append({"parameter": param, "message": message})
+
+    def has_errors(self):
+        return len(self.errors) > 0
+
+    def __bool__(self):
+        return False
+
+
+class AnalyzeCreateValidRequest:
+    def __init__(self, parameters=None):
+        self.parameters = parameters
+
+    def __bool__(self):
+        return True
+
+
+def build_analyze_request(parameters=None):
+    accepted_parameters = ["symbol", "start_date", "end_date"]
+    invalid_req = AnalyzeCreateInvalidRequest()
+
+    if parameters is not None:
+        if not isinstance(parameters, dict):
+            invalid_req.add_error("parameters", "Is not iterable")
+            return invalid_req
+
+        if len(parameters) != 3:
+            invalid_req.add_error("parameters", "missing parameter")
+            return invalid_req
+
+        for key, value in parameters.items():
+            if key not in accepted_parameters:
+                invalid_req.add_error("parameters", "Key {} cannot be used".format(key))
+                return invalid_req
+
+    else:
+        invalid_req.add_error("parameters", "Need 3 parameters: symbol, start_date, end_date. Got 0.")
+        return invalid_req
+
+    return AnalyzeCreateValidRequest(parameters=parameters)
---
+++ pbrenko_trials/tests/requests/test_analyze.py
@@ -0,0 +1,35 @@
+from pbrenko_trials.requests.analyze import build_analyze_request
+
+
+def test_build_analyze_request_without_parameters():
+    request = build_analyze_request()
+
+    assert request.has_errors()
+    assert bool(request) is False
+
+
+def test_build_analyze_request_missing_parameters():
+    request = build_analyze_request({"symbol": "BTCUSDT", "start_date": "202101010000"})
+
+    assert request.has_errors()
+    assert bool(request) is False
+
+
+def test_build_analyze_request_with_invalid_keys():
+    request = build_analyze_request({"sybol": "BTCUSDT", "start_date": "202101010000", "end_date": "202301010000"})
+
+    assert request.has_errors()
+    assert bool(request) is False
+
+
+def test_build_analyze_request_with_invalid_parameters():
+    request = build_analyze_request("test")
+
+    assert request.has_errors()
+    assert bool(request) is False
+
+
+def test_build_analyze_request_with_valid_parameters():
+    request = build_analyze_request({"symbol": "BTCUSDT", "start_date": "202101010000", "end_date": "202301010000"})
+
+    assert bool(request) is True
