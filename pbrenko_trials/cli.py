296a96d50d43b535b36132fd72a6a36d9126074f
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2023-07-14 22:30:02 +0000

    bug fixes for cli

--- pbrenko_trials/cli.py
+++ pbrenko_trials/cli.py
@@ -6,7 +6,9 @@ import json
 from pbrenko_trials.repository.memrepo import MemRepo
 from pbrenko_trials.repository.binance.binancerepo import BinanceRepo
 from pbrenko_trials.use_cases.analyze import AnalyzeUseCase
+from pbrenko_trials.use_cases.backtest import BacktestUseCase
 from pbrenko_trials.requests.analyze import build_analyze_request
+from pbrenko_trials.requests.backtest import build_backtest_request
 
 APPLICATION_CONFIG_PATH = "config"
 
@@ -21,6 +23,19 @@ def analyze(symbol, repo_type, interval, start_date, end_date):
 
     analyze_use_case = AnalyzeUseCase()
     result = analyze_use_case.analyze(repo, request)
+    print(result.value)
+
+
+def backtest(symbol, repo_type, percent, interval, start_date, end_date):
+    if repo_type == "test":
+        repo = MemRepo()
+    elif repo_type == "crypto":
+        repo = BinanceRepo()
+
+    request = build_backtest_request({"symbol": symbol, "percent": percent, "interval": interval, "start_date": start_date, "end_date": end_date})
+
+    backtest_use_case = BacktestUseCase()
+    result = backtest_use_case.backtest(repo, request)
     print(result.value)
 
 
@@ -58,6 +73,7 @@ if __name__ == "__main__":
         interval = sys.argv[5]
         start_date = sys.argv[6]
         end_date = sys.argv[7]
+        analyze(symbol, repo_type, interval, start_date, end_date)
     elif sys.argv[2] == "backtest":
         symbol = sys.argv[3]
         repo_type = sys.argv[4]
@@ -65,5 +81,5 @@ if __name__ == "__main__":
         interval = sys.argv[6]
         start_date = sys.argv[7]
         end_date = sys.argv[8]
+        backtest(symbol, repo_type, percent, interval, start_date, end_date)
     configure_app(config)
-    analyze(symbol, repo_type, interval, start_date, end_date)
