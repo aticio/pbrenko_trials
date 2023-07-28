fcc6943a0b5166fc373776ec3219d5bcb4860328
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2023-07-28 21:35:10 +0000

    chart drawing implemented as a tool for use cases

---
+++ pbrenko_trials/pbrenko_trials/use_cases/tools/draw_chart.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import numpy as np
+import os
+
+
+def draw_chart(bricks, percent, file_name):
+    chart_path = os.getenv("CHART_PATH")
+    min_val = bricks[0].close
+    largest_brick_size = 0
+    for b in bricks:
+        brick_size = abs(b.close - b.open)
+        if brick_size > largest_brick_size:
+            largest_brick_size = brick_size
+
+        if b.close < min_val:
+            min_val = b.close
+
+    brick_width = largest_brick_size / 2
+    y_max = 0
+    fig, ax = plt.subplots()
+
+    count = 1
+    for b in bricks:
+        y = 0
+        color = ""
+        if b.type == "up":
+            y = b.open
+            color = "green"
+        elif b.type == "down":
+            y = b.close
+            color = "red"
+        else:
+            color = "gray"
+            y = b.close
+
+        if y > y_max:
+            y_max = y
+
+        brick_size = (b.close * percent / 100)
+        r = Rectangle((count * brick_width, y), brick_width, brick_size)
+        r.set_color(color)
+
+        ax.add_patch(r)
+        count = count + 1
+
+    ax.set_xlim(0, count * brick_width)
+    ax.set_ylim(min_val - brick_width, y_max + (y_max * 0.02))
+    ax.set_axisbelow(True)
+    ax.get_xaxis().set_visible(False)
+
+    ticks = np.arange(min_val - brick_width, y_max + (y_max * 0.02), brick_width * 2)
+    plt.yticks(ticks)
+    plt.grid(linestyle='--', color="#ccd8c0")
+    plt.savefig(chart_path + "/" + file_name)
---
+++ pbrenko_trials/tests/use_cases/tools/test_draw_chart.py
@@ -0,0 +1,79 @@
+from pbrenko_trials.domain.brick import Brick
+from pbrenko_trials.use_cases.tools.draw_chart import draw_chart
+from os.path import exists
+from unittest import mock
+import os
+
+
+@mock.patch.dict(os.environ, {"CHART_PATH": "/workspaces/pbrenko_trials/pbrenko_trials/tests/use_cases/tools/images"})
+def test_draw_chart():
+    brick_1 = Brick(
+        type="up",
+        open=100,
+        close=100,
+        high=110,
+        low=100,
+    )
+
+    brick_2 = Brick(
+        type="up",
+        open=110,
+        close=121,
+        high=125,
+        low=110,
+    )
+
+    brick_3 = Brick(
+        type="up",
+        open=121,
+        close=133.1,
+        high=133.1,
+        low=121,
+    )
+
+    brick_4 = Brick(
+        type="up",
+        open=133.1,
+        close=146.1,
+        high=150,
+        low=133.1,
+    )
+
+    brick_5 = Brick(
+        type="down",
+        open=131.769,
+        close=118.5921,
+        high=140,
+        low=118.5921,
+    )
+
+    brick_6 = Brick(
+        type="down",
+        open=118.5921,
+        close=106.73289,
+        high=118.5921,
+        low=105,
+    )
+
+    brick_7 = Brick(
+        type="up",
+        open=117.406179,
+        close=129.1467969,
+        high=135,
+        low=117.406179,
+    )
+
+    brick_8 = Brick(
+        type="up",
+        open=129.1467969,
+        close=142.06147659,
+        high=145,
+        low=129.1467969,
+    )
+
+    bricks = [brick_1, brick_2, brick_3, brick_4, brick_5, brick_6, brick_7, brick_8]
+    percent = 10
+
+    draw_chart(bricks, percent, "chart.png")
+    file_exists = exists("/workspaces/pbrenko_trials/pbrenko_trials/tests/use_cases/tools/images/chart.png")
+    assert file_exists is True
