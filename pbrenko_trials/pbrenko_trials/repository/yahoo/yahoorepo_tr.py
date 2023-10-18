dea623a39f4e7aaac92c125c69c68696adea22ec
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2023-10-18 14:30:00 +0000

    repo change for stock-tr

--- pbrenko_trials/pbrenko_trials/repository/yahoo/yahoorepo_tr.py
+++ pbrenko_trials/pbrenko_trials/repository/yahoo/yahoorepo_tr.py
@@ -10,7 +10,7 @@ class YahooRepoTR:
         return data.values.tolist()
 
     def list_pairs(self):
-        tickers = pd.read_html("https://tr.wikipedia.org/wiki/Borsa_%C4%B0stanbul#2023_itibar%C4%B1yla_BIST100'de_i%C5%9Flem_g%C3%B6ren_%C5%9Firketler")[2]
+        tickers = pd.read_html("https://tr.wikipedia.org/wiki/Borsa_%C4%B0stanbul#2023_itibar%C4%B1yla_BIST100'de_i%C5%9Flem_g%C3%B6ren_%C5%9Firketler")[1]
         symbol_list = tickers["Kod [9]"].values.tolist()
         corrected_symbol_list = []
         for symbol in symbol_list:
